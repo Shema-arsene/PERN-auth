a2384a6da4e93a0bfdb6ae3f559b81abee4c1b41
Shema Arsene <aniyonkurushema@gmail.com>
2025-12-11 13:12:17 +0200

    Server setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookiesParser from "cookie-parser"
 
 dotenv.config()
 
 const app = express()
+
+app.use(express.json())
+app.use(cookiesParser())
 
 app.get("/", (req, res) => {
   res.send("Hello World!")
