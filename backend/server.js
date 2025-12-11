a13054ae1c1f3cb03535c5450cf4b830958adb29
Shema Arsene <aniyonkurushema@gmail.com>
2025-12-11 15:07:13 +0200

    Registering the routes in the serrver

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,23 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookiesParser from "cookie-parser"
+import cors from "cors"
+import authRoutes from "./routes/auth.js"
 
 dotenv.config()
 
 const app = express()
 
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+)
 app.use(express.json())
 app.use(cookiesParser())
 
-app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
+app.use("/api/auth", authRoutes)
 
 const PORT = process.env.PORT || 3000
 
