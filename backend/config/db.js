1f80deb25651f788e941b70460456d6a60561f6b
Shema Arsene <aniyonkurushema@gmail.com>
2025-12-11 13:05:06 +0200

    Database connection

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+import { Pool } from "pg"
+import dotenv from "dotenv"
+
+dotenv.config()
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+})
+
+pool.on("connect", () => {
+  console.log("Database Connected")
+})
+
+pool.on("error", (err) => {
+  console.error("Unexpected error", err)
+})
+
+export default pool
